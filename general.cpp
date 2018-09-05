3fd1d86228e49a0154f33a1e39eaa9064c39b2e9
Zachary Damato <zacharyd1@live.com>
2018-09-05 11:50:33 -0400

    Created the 'general' module and the timer class within it

---
+++ general.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <chrono>	//std::chrono::high_resolution_clock
+
+#include "zlib/general.h"
+
+//So we can assume zlib is the namespace incase we need to pull things from other zlib files
+namespace zlib {}
+using namespace zlib;
+
+namespace zlib
+{
+	timer::timer()
+	{
+		begin = std::chrono::high_resolution_clock::now();
+	}
+
+	double timer::getTime()
+	{
+		//The result is divided by 10^9 to convert from ns to s
+		return (std::chrono::high_resolution_clock::now() - begin).count() / pow(10, 9);
+	}
+}
