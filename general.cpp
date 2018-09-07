28bf350030ed55b4514439482747d5be6b8e43d2
Zachary Damato (Polaris1) <zacharyd1@live.com>
2018-09-07 15:48:28 -0400

    Added a flag to change the includes from 'zlib/general.h' to 'general.h'

--- general.cpp
+++ general.cpp
@@ -1,7 +1,12 @@
 #pragma once
 #include <chrono>	//std::chrono::high_resolution_clock
+#include <cmath>
 
+#ifndef makefile
 #include "zlib/general.h"
+#else
+#include "general.h"
+#endif
 
 //So we can assume zlib is the namespace incase we need to pull things from other zlib files
 namespace zlib {}
@@ -17,6 +22,6 @@ namespace zlib
 	double timer::getTime()
 	{
 		//The result is divided by 10^9 to convert from ns to s
-		return (std::chrono::high_resolution_clock::now() - begin).count() / pow(10, 9);
+		return (std::chrono::high_resolution_clock::now() - begin).count() / std::pow(10, 9);
 	}
 }
