8aecb2027c78b3f5cd79045320226cf67b81e46d
Zachary Damato <zacharyd1@live.com>
2018-10-03 16:08:09 -0400

    Added some required dll's to network.cpp

--- network.cpp
+++ network.cpp
@@ -3,6 +3,11 @@
 #include <WinSock2.h>
 #include <WS2tcpip.h>
 
+// Need to link with Ws2_32.lib, Mswsock.lib, and Advapi32.lib
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
+
 #include "network.h"
 
 namespace zlib
