b345cc65e3edc1386df80d097aa52883fff97cf1
Zachary Damato <zacharyd1@live.com>
2018-12-08 03:38:13 -0500

    Added tests.cpp, a file to contain and excute tests for the submodules' classes and functions.

---
+++ tests.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+#include "zlib.h"
+
+int main(int argc, char * argv[])
+{
+	cout << "Beginning tests." << endl;
+
+
+
+	
+	cout << "Ending tests." << endl;
+	return 0;
+}
