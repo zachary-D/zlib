092594f63182cd2772bc67f7e48a115cb6b17c1e
Zachary Damato <zacharyd1@live.com>
2018-12-08 02:08:57 -0500

    Merged varConv into varTypes (it was causing compilation issues on Linux)

--- varTypes.h
+++ varTypes.h
@@ -13,6 +13,8 @@
 #ifdef __linux__
 #include <math.h>	//Needed for pow() on linux
 #endif
+#include<time.h>
+#include<sstream>
 
 using std::string;
 using std::vector;
@@ -1146,4 +1148,57 @@ namespace zlib
 		};
 
 	};
+
+	namespace conv
+	{
+		enum class convertError
+		{
+			convertFailed,
+		};
+
+		template<class T>
+		string toString(T inp)
+		{
+			stringstream convert;
+			string out;
+
+			convert.clear();
+			convert << inp;
+
+			convert >> out;
+			if (convert.fail()) throw convertError::convertFailed;
+			return out;
+		}
+
+		string toString(var::coord2 inp, bool multiLine = true);
+		string toString(var::color_RGB inp, bool multiLine = true);
+		string toString(bool inp);
+		string toString(var::longTime time);	//Leaves off values if they are 0, starting from years down to the smallest value that is not 0.  (At minimum, 0 seconds will be returned)
+
+		string toLowercase(string & inp, bool changeArg = true);	//Coverts 'inp' to lowercase.  USES POINTERS TO CHANGE ARGUMENT VALUES WHEN 'changeArg' IS TRUE
+		char toLowercase(char & inp, bool changeArg = false);		//Coverts 'inp' to lowercase.  USES POINTERS TO CHANGE ARGUMENT VALUES WHEN 'changeArg' IS TRUE
+
+		string toUppercase(string & inp, bool changeArg = true);	//Coverts 'inp' to uppercase.  USES POINTERS TO CHANGE ARGUMENT VALUES WHEN 'changeArg' IS TRUE
+		char toUppercase(char & inp, bool changeArg = false);		//Coverts 'inp' to uppercase.  USES POINTERS TO CHANGE ARGUMENT VALUES WHEN 'changeArg' IS TRUE
+
+		bool isNum(string inp);
+		bool isNum(char inp);
+
+		double toNum(string inp);
+		double toNum(char inp);
+
+		string toHex(unsigned value);
+
+		bool isBool(string inp);
+
+		bool toBool(string inp);
+		bool toBool(double inp);
+
+#ifdef ZLIB_USING_CINDER
+		var::coord2 toCoord2(glm::highp_vec2 coordinate);
+#endif
+
+		double toDegrees(double radians);
+		double toRadians(double degrees);
+	}
 }
